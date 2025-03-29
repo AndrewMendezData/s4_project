3d0c6180b181436817d4b748b13303205ea71448
Andrew Mendez <andrewmendez@AndrewsM1Air.local>
2025-03-28 19:15:16 -0600

    Final commit before project submission.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # s4_project
+
+This project is to explore data about vehicles for sale in the US.
+
+Libraries Used:
+* pandas
+* streamlit
+* plotly.express
+* 
+
+To Launch Project on your local machine:
+1. Download the repository to your local machine.
+2. From your Command Line Interface (CLI), make sure you have the **streamline** package installed (if not, use "pip install streamline")
+3. Type "streamlit run app.py", and the web address that appears will take you to the application.
---
+++ app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+vehicles = pd.read_csv('/Users/andrewmendez/t10/s4_project/vehicles_us.csv')
+
+
+# Histogram Comparing Prices Between Car Models
+
+st.header('Car Models & Prices Comparison')
+
+# List of car models
+model_list = sorted(vehicles['model'].unique())
+
+# User input for Car 1
+model_1 = st.selectbox(
+                    label='Select Model 1',
+                    options=model_list,
+                    index=model_list.index('ford f-150')
+)
+
+# User input for Car 2
+model_2 = st.selectbox(
+                    label='Select Model 2',
+                    options=model_list,
+                    index=model_list.index('toyota corolla')
+)
+
+# Filtering the dataframe
+mask_filter = (vehicles['model'] == model_1) | (vehicles['model'] == model_2)
+vehicles_filtered = vehicles[mask_filter]
+
+# Checkbox for user to normalize histogram
+normalize = st.checkbox('Normalize Histogram', value=True)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
+
+# Creating histogram
+fig1 = px.histogram(vehicles_filtered,
+                    x='price',
+                    nbins=100,
+                    color='model',
+                    histnorm=histnorm,
+                    barmode='overlay')
+
+# Displaying with Streamlit
+st.write(fig1)
+
+
+
+
+# Scatterplot: Relationship between Price & Model Year
+
+st.header('Price & Model Year Relationship')
+
+fig2 = px.scatter(
+    vehicles, 
+    x='price', 
+    y='odometer',
+    width=1200,
+    height=800
+    )
+
+fig2.update_xaxes(range=[0, 80000])
+fig2.update_yaxes(range=[0,500000], dtick=50000)
+
+st.plotly_chart(fig2)
